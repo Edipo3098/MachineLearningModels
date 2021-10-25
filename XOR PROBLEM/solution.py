215baefbc54bdfc9cd9fdf3d913eb11720a734e4
Edipo3098 <felipe.porras.014@gmail.com>
2021-10-25 10:47:38 -0500

    Add a neural network to classify the  IRIS dataset classification problem

--- XOR PROBLEM/solution.py
+++ XOR PROBLEM/solution.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import math 
-import random
+import matplotlib.pyplot as plt
 # IMPORT THE DATA
 X = np.array([(0,0),(0,1),(1,0),(1,1)])
 Y = np.array([0,1,1,0])
@@ -16,7 +16,7 @@ X = np.append(X,np.ones((N2,1))*-1,axis=1)
 Nhl  = 2
 Number_weights_per_neurom = D2+1 # The bias weight is taking care with the 1
 weights_hidden_layer = Number_weights_per_neurom*Nhl
-Whl = np.random.rand(D2,Number_weights_per_neurom)
+Whl = np.random.rand(Nhl,Number_weights_per_neurom)   # matrix   2x 3
 Output = np.zeros((Nhl,1))
 Sensitividad_hl = np.zeros(((Nhl,1) ))
 Sensitividad2_hl = np.zeros(((Nhl,1) ))
@@ -27,21 +27,21 @@ dp_hl = np.zeros((D2,Number_weights_per_neurom),dtype=float)
 Nol = 1
 Number_weights_per_neurom_ol = Nhl+1
 weights_output_layer = Number_weights_per_neurom_ol*Nol
-Wol =   np.random.rand(Nol,Number_weights_per_neurom)
+Wol =   np.random.rand(Nol,Number_weights_per_neurom_ol)
 Outpu_ol = np.zeros(1)
 Sensitividad_ol = np.zeros(((Nol,1) ))
 Sensitividad2_ol = np.zeros(((Nol,1) ))
 Senns_ol = np.zeros(((Nol,1) ))
-dp_ol = np.zeros((Nol,Number_weights_per_neurom))   # delta weith
+dp_ol = np.zeros((Nol,Number_weights_per_neurom_ol))   # delta weith
 error_ol =  np.zeros(((Nol,1) ))
 
 alpha = 0.1 #learning factor
 
 #Training
-print (Whl)
-print(Wol)
+
 error_d = 0.01 #Desired error
 E_p = 100 #Actual error
+E_total =[]
 Error_iteracion = 0
 epocas = 0
 #Una epoca manual probar
@@ -54,21 +54,27 @@ while E_p >= error_d:
 	for i in range(0,N2):
 		#Recorrido hacia adelante 
 		#Output hidden layer 
-		aux1 = Whl[0,0]*X[i,0]  +Whl[0,1]*X[i,1]   +  Whl[0,2]*X[i,2]
-		aux2 = Whl[1,0]*X[i,0]  +Whl[1,1]*X[i,1]   +  Whl[1,2]*X[i,2]
+		#aux1 = Whl[0,0]*X[i,0]  +Whl[0,1]*X[i,1]   +  Whl[0,2]*X[i,2]
+		#aux2 = Whl[1,0]*X[i,0]  +Whl[1,1]*X[i,1]   +  Whl[1,2]*X[i,2]
+		aux =  np.sum(Whl*X[i,:],axis=1)
 
-		Output[0] =  1/(1+np.exp(-aux1))
-		Output[1] =  1/(1+np.exp(-aux2))
-
+		#Output[0] =  1/(1+np.exp(-aux[0]))
+		#Output[1] =  1/(1+np.exp(-aux[1]))
+		Output =  1/(1+np.exp(-aux))
+		Output = Output.reshape((-1,1))
 		#print(Output)
 
+		
 
-		#Output last layer
+
+		#Output last layer calculate the entries for the last later
 		Xol = []
 		Xol = np.append(Output,np.ones((Nol,1))*-1 , axis=0)
 
 		aux3 = Wol[0,0]*Xol[0]  + Wol[0,1]*Xol[1] + Wol[0,2]*Xol[2] 
-		Outpu_ol = 1/(1+ math.exp(-aux3))
+		#aux3 =np.sum(Wol*Xol)
+		
+		Outpu_ol = 1/(1+ np.exp(-aux3))
 
 		#print("Salida " +  str(Outpu_ol) +    " deseado  " + str(Y[i]))
 
@@ -88,6 +94,8 @@ while E_p >= error_d:
 		dp_ol[0,0] = alpha*Xol[0,0]*Sensitividad_ol
 		dp_ol[0,1] = alpha*Xol[1,0]*Sensitividad_ol
 		dp_ol[0,2] = alpha*Xol[2,0]*Sensitividad_ol
+		#dp_ol = alpha*Xol*Sensitividad_ol
+		
 
 
 
@@ -114,12 +122,14 @@ while E_p >= error_d:
 		Whl[1,2] = Whl[1,2] + dp_hl[1,2] 
 
 		#outpu layer
+		#Wol = Wol + dp_ol
 		Wol[0,0] = Wol[0,0] + dp_ol[0,0] 
 		Wol[0,1] = Wol[0,1] + dp_ol[0,1] 
 		Wol[0,2] = Wol[0,2] + dp_ol[0,2] 
 		#END iteration
 	
 	E_p = math.sqrt(Error_iteracion)/N2
+	E_total.append(E_p)
 	print(E_p)
 	#print(epocas)
 	epocas+=1
@@ -127,6 +137,15 @@ while E_p >= error_d:
 
 #prueba de que funciona la red
 print("Entrenamiento finalizado......Se demoro "+ str(epocas))
+ax= plt.subplot()
+Epocas = range(epocas)
+ax.plot(Epocas, E_total)
+ax.set(xlabel='Epocs', ylabel='Cuadratic error',
+       title='Neural net error XOR problem')
+ax.grid()
+
+
+plt.show()
 
 print("Inicia test")
 for i in range(0,N2):
